f9ee9ffa9e9b6fec8967c0b011ad4fd0eedd03c9
maheshthippala <mahesh.cubex@gmail.com>
2018-07-18 06:58:31 +0530

    Initial commit

---
+++ app/src/main/java/cubex/mahesh/uiexample_july6am/MainActivity.kt
@@ -0,0 +1,20 @@
+package cubex.mahesh.uiexample_july6am
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun getText(v:View){
+
+               tv1.text = et1.text
+    }
+
+}
